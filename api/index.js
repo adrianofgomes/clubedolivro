af498d97caef6fd86077be7eba4c9d99896c82ba
Adriano Gomes <adrianofgomes@gmail.com>
2019-06-25 22:12:39 -0300

    Teste API

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@ const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 admin.initializeApp();
 
-exports.main = functions.https.onRequest(async(req, res) => {
+exports.main = functions.https.onRequest((req, res) => {
   const db = admin.firestore();
   const data = [{ 'nome': 'abc' }, { 'nome': 'def' }];
 
-  data.forEach(function(obj){
+  data.forEach(async function(obj){
     const ref = await db.collection('notes').add(data);
     console.log("Document written with ID: ", ref.id);
   })
