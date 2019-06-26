53fb74cef63b869fd61a83b778c9658e204f8399
Adriano Gomes <adrianofgomes@gmail.com>
2019-06-26 12:50:48 -0300

    API Livro

--- api/index.js
+++ api/index.js
@@ -6,20 +6,45 @@ exports.main = functions.https.onRequest(async(req, res) => {
   console.log("iniciando função main");
   const db = admin.firestore();
 
-  const data = { 'teste': 'teste' };
-  const ref = await db.collection('notes').add(data);
+  //const data = { 'teste': 'teste' };
+  //const ref = await db.collection('notes').add(data);
 
-  const dataList = [{ 'nome': 'abc' }, { 'nome': 'def' }];
+  /*const dataList = [{ 'nome': 'abc' }, { 'nome': 'def' }];
 
   dataList.forEach(async function(obj){
     console.log("inserindo obj: " + obj);
     const outraRef = await db.collection('outra').add(obj);
     console.log("Document written with ID: ", outraRef.id);
-  })
-  
+  })*/
+
+  const livros = require('./livro/livro.json');
+
+  //Se a coleção de livros estiver vazia, carregar livros a partir do json
+  console.log('Inicialização: verificando se precisar carregar lista de livros...');
+  if(db){
+
+    dataList.forEach(async function(livro){
+      const livroRef = await db.collection('livros').add(livro);
+      console.log("Carregado livro: ", livroRef.id + " - " + livroRef.nome);
+    });
+
+    /*var colecaoLivros = db.collection('livros');
+    colecaoLivros.count(function(err, count){
+      if(count === 0){
+        colecaoLivros.insertMany(livros, function(err, r){
+          if(err){
+            console.log('erro ao inserir livros');
+          }
+          console.log('livros incluídos com sucesso: ' + r.insertedCount);
+        });
+      } else {
+        console.log('coleção de livros já está carregada. não foi realizada nova carga de livros.')
+      }
+    });*/
+  }
+
   res.json({
-      'status': 'OK',
-      'ref': ref.id
+      'status': 'OK2',
   });
   
 });
