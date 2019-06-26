fcec14ce6ecffd5424bd2d974afa0e42db44140c
Adriano Gomes <adrianofgomes@gmail.com>
2019-06-25 21:10:43 -0300

    Teste de conexão Function X Cloud Store

--- function1/index.js
+++ function1/index.js
@@ -1,3 +1,7 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+admin.initializeApp();
+
 /**
  * Responds to any HTTP request.
  *
@@ -10,49 +14,15 @@ exports.helloWorld = (req, res) => {
 };
 
 exports.main = (req, res) => {
-  const admin = require('firebase-admin');
-  const functions = require('firebase-functions');
-
-  console.log('inicializando firebase');
-  admin.initializeApp(functions.config().firebase);
-  
   var db = admin.firestore();
 
-  const ttl = Number.parseInt(123);
-  const ciphertext = ('abcdef');
-  const created = new Date().getTime();
-
-  db.collection('users').add({ created, ttl, ciphertext })
-  .then(doc => {
-    return res.status(200).send(doc);
-  }).catch(err => {
-    console.error(err);
-    return res.status(404).send({ error: 'unable to store', err });
+  const db = admin.firestore();
+  const data = { 'teste': 'teste' };
+  const ref = await db.collection('notes').add(data);
+  res.json({
+      id: ref.id,
+      data
   });
-
-  /*var docRef = db.collection('users').doc('alovelace');
-
-  var setAda = docRef.set({
-    first: 'Ada',
-    last: 'Lovelace',
-    born: 1815
-  });
-
-  db.collection('users').get()
-    .then((snapshot) => {
-      console.log('recuperando documentos');
-      snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-      });
-      res.status(200).send(message);
-    })
-    .catch((err) => {
-      console.log('Error getting documents', err);
-      res.status(500).send('Error getting documents\n\n' + err);
-    });
   
-  let message = req.query.message || req.body.message || 'Hello from main!';
-  res.status(200).send(message);*/
-
 };
 
